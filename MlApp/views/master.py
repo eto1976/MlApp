6c260bf786fc6d2c1434b5a0dea7bf2e7c24511c
1506 <1506@OI-17001.eastrise.local>
2020-01-23 19:00:01 +0900

    マスタ機能追加中

--- MlApp/views/master.py
+++ MlApp/views/master.py
@@ -6,18 +6,43 @@ from MlApp.models.mstimagelabel import Mst_imagelabel
 from MlApp.forms.toolsform import ToolsForm
 
 
-# 実行またはリロード処理
+# 処理実行またはリロード処理
 def masterExecution(request):
 
-    # 実行ボタン押下時
+    # 検索ボタン押下時
     if 'doSearch' in request.POST:
         # 検索処理
         imagelabelList = Mst_imagelabel.objects.all()
-        #ディクショナリーの形で設定
 
-    template = loader.get_template("master.html")
-    context = {
-        "imagelabelList": imagelabelList,
-    }
+        template = loader.get_template("master.html")
+        context = {
+            "imagelabelList": imagelabelList,
+        }
+
+    # 登録ボタン押下時
+    elif 'doInsert' in request.POST:
+        imagelabelList = Mst_imagelabel.objects.all()
+
+        template = loader.get_template("masterEdit.html")
+        context = {
+        }
+
+    # 修正ボタン押下時
+    elif 'doEdit' in request.POST:
+        imagelabelList = Mst_imagelabel.objects.all()
+
+        template = loader.get_template("masterEdit.html")
+        context = {
+        }
+
+    # 削除ボタン押下時
+    elif 'doDelete' in request.POST:
+        imagelabelList = Mst_imagelabel.objects.all()
+
+        template = loader.get_template("master.html")
+        context = {
+            "imagelabelList": imagelabelList,
+        }
+
 
     return HttpResponse(template.render(context, request))
