38b8eb6b8590156cc8822a9a9af6fb8b2c8a946b
1506 <1506@OI-17001.eastrise.local>
2020-02-12 17:11:05 +0900

    デバッグモード対応中

--- MLWebApp/settings.py
+++ MLWebApp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'p%p=g^s!uok=tx7ac-0m97ukv9d!zlmotzwbdror#pivoz++$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -167,3 +167,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
