73425b269905d4d3d3890fd36aff338fdbd65d64
1506 <1506@OI-17001.eastrise.local>
2020-03-17 16:46:02 +0900

    ライブラリの追加

--- MlApp/batch/__init__.py
+++ MlApp/batch/__init__.py
@@ -1,3 +1,4 @@
 from MlApp.batch.imagelearnLogic import ImagelearnLogic
 from MlApp.batch.webCrawlerLogic import WebCrawlerLogic
 from MlApp.batch.googleWebCrawlerLogic import GoogleWebCrawlerLogic
+from MlApp.batch.google_images_download import googleimagesdownload
